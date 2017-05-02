1ae37e829ddd094b3eda27960649fa3d3c5e3939
LostWyvern <vikingsean77@aim.com>
2017-05-02 12:31:48 -0400

    Added Program

---
+++ Programming Task/ProgrammingTask/WebForm1.aspx
@@ -0,0 +1,57 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="ProgrammingTask.WebForm1" %>
+
+<!DOCTYPE html>
+
+<style type="text/css"> 
+    #container {     
+        display:table;     
+        border-collapse:collapse;   
+        height:200px;   
+        width:100%; 
+    }          
+    #layout {     
+        display:table-row;    
+    }            
+    #content {     
+        display:table-cell;   
+        text-align:center;  
+        vertical-align:middle;     
+    }            
+    #movieText {
+     width:500px;
+     margin:0px auto;
+     text-align:left;
+     padding:15px;
+    }
+</style>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Programming Task</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div id ="container">
+        <div id ="layout">
+            <div id ="content">
+                 
+                <div id ="movieText">
+                    Enter the title of a movie you'd like to search for!
+                    <br />
+                    <br />
+                    <asp:textbox ID="TextBoxMovieName" runat="server"></asp:textbox>
+                    <br />
+                    <br />
+                    <asp:Button ID="ButtonSearch" runat="server" Text="Search" OnClick="ButtonSearch_Click" />
+                    <br />
+                    <br />
+                    <br />
+                    <asp:label ID="LabelResponse" runat="server" text=""></asp:label>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Programming Task/ProgrammingTask/WebForm1.aspx.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace ProgrammingTask
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void ButtonSearch_Click(object sender, EventArgs e)
+        {
+            string movieRequest = "http://www.omdbapi.com/?t=" + HttpContext.Current.Server.UrlEncode(TextBoxMovieName.Text); //Create the URL String and code it as a URL
+
+            HttpWebRequest webRequest = (HttpWebRequest) WebRequest.Create(movieRequest); //Make the HTTP Request with the generated string
+
+            webRequest.Timeout = 10000;
+            webRequest.UserAgent = "Code Sample Web Client";
+
+            HttpWebResponse webResponse = (HttpWebResponse)webRequest.GetResponse();
+
+            Encoding enc = Encoding.GetEncoding(1252);
+
+            StreamReader responseStream = new StreamReader(webResponse.GetResponseStream(), enc);
+
+            string movieData = responseStream.ReadToEnd(); //Create the string that holds the response.
+
+            webResponse.Close();
+            responseStream.Close();
+
+            if (movieData == "{\"Response\":\"False\",\"Error\":\"Movie not found!\"}")
+            {
+                LabelResponse.Text = "Sorry! The movie you were looking for was not found! Please Try Again!"; //If the API data comes back with movie not found, display a sorry message
+                TextBoxMovieName.Text = "";
+            }
+            else
+            {
+                movieData = movieData.Replace("\",\"", "<br />");
+                movieData = movieData.Replace("\"", "");
+                movieData = movieData.Replace("{", "");             //Remove unwanted characters from the data retrieved from the API. Add spacing to make it clearer
+                movieData = movieData.Replace("}", "");
+                movieData = movieData.Replace(":", ": ");
+                movieData = movieData.Replace("[],", "<br />");
+                movieData = movieData.Replace("Response: True", "");
+                movieData = movieData.Replace(",", ", ");
+                movieData = movieData.Replace("[", " ");
+                movieData = movieData.Replace("]", " ");
+
+                TextBoxMovieName.Text = "";
+
+                LabelResponse.Text = movieData;
+            }
+
+        }
+    }
+}
